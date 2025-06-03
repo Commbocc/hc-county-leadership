0294ae695a8dfd1089330b59b261593d4ea6c7bd
mccarthyk <mccarthyk@hcflgov.net>
2025-06-03 13:12:06 -0400

    fix/riframeresizer-baseurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,18 @@
+const baseURL = "/hc-county-leadership";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
   css: ["~/assets/css/main.css"],
+
   app: {
-    baseURL: "/hc-county-leadership/",
+    baseURL,
     head: {
       script: [
         {
-          src: "/iframeResizer.contentWindow.min.js",
+          src: `${baseURL}/iframeResizer.contentWindow.min.js`,
           async: true,
         },
       ],
